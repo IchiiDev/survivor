bc27917ec6c4a4c058e0cada82fe3e5f5599a6c8
Baptiste <baboufe@gmail.com>
2024-09-04 12:34:59 +0200

    feature: added sidebar with icons, borders

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,5 +1,6 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./pages.scss"
+import Sidenav from "../components/Sidenav"
 
 const Layout = () => {
   function manageNav() {
@@ -9,26 +10,19 @@ const Layout = () => {
       if (sidenav.style.width === "250px") {
         sidenav.style.width = "0px";
         docnav.style.marginLeft = "0px";
+        sidenav.style.borderRight = "0px solid #9e9a9a";
       } else {
         sidenav.style.width = "250px";
         docnav.style.marginLeft = "250px";
+        sidenav.style.borderRight = "2px solid #9e9a9a";
       }
     }
   }
   return (
     <div>
-      <div id="mySidenav" className="sidenav">
-        <Link onClick={manageNav} to="/">Home</Link>
-        <Link onClick={manageNav} to="/compatibility">Compatibility</Link>
-        <Link onClick={manageNav} to="/wardrobe">Wardrobe</Link>
-        <Link onClick={manageNav} to="/coaches">Coaches</Link>
-        <Link onClick={manageNav} to="/customers">Customers</Link>
-        <Link onClick={manageNav} to="/statistics">Statistics</Link>
-        <Link onClick={manageNav} to="/tips">Tips</Link>
-        <Link onClick={manageNav} to="/events">Events</Link>
-      </div>
+      <Sidenav manageNav={manageNav} />
       <nav className="navbar" role="navigation">
-        <img className="navbar-item" onClick={manageNav} src="https://w7.pngwing.com/pngs/267/362/png-transparent-computer-icons-encapsulated-postscript-others-miscellaneous-button-icon-burger-menu.png" alt="Menu"></img>
+        <img onClick={manageNav} className="navbar-item" src="/assets/icon-menu.svg" alt="menu"/>
       </nav>
       <div id="main">
         <Outlet />
