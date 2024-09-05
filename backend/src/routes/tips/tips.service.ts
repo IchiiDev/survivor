af0021996cd6f484d71ce71a48b3c532ce7c63b1
karumapathetic <arthur.lassausaie@epitech.eu>
2024-09-05 10:19:23 +0200

    backend-tips
    All routes are now functionnal

--- backend/src/routes/tips/tips.service.ts
+++ backend/src/routes/tips/tips.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { db } from "../../main";
 
 @Injectable()
 export class TipsService {
-    getAllTips(): string {
-        return 'This service returns all tips';
+    async getAllTips(): Promise<{ id: number, title: string, tip: string }[] | null> {
+        const result = await db.query('SELECT * FROM tips');
+
+        if (Array.isArray(result[0])) {
+            return result[0].map((row: any) => ({
+                id: row.id,
+                title: row.title,
+                tip: row.tip,
+            }));
+        }
+
+        return null;
     }
 
-    createTip(): string {
-        return 'This service creates a new tip';
+    async createTip(title: string, tip: string): Promise<string> {
+        await db.query(
+            'INSERT INTO tips (title, tip) VALUES (?, ?)',
+            [title, tip],
+        );
+        return `New tip created.`;
     }
 }
 
