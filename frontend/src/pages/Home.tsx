604c81b31dbe47610edf6f44db5a351ab1fe6598
Anthony <anthony.ardhuin@epitech.eu>
2024-09-06 10:36:56 +0200

    feat : Home basics stats with box & table, basic statistics Pie & Line

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,97 +1,57 @@
 import Titlebox from "../components/Titlebox"
-import './Home.scss';
-import { Line } from 'react-chartjs-2';
-import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement,
-		Title, Tooltip, Legend, } from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement,
-	Title, Tooltip, Legend );
+import "./Stats.scss";
 
 const Home = () => {
-	const newCustomers = {
-		labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-		datasets: [{
-			label: 'Statistiques',
-			data: [65, 59, 80, 81, 56, 55],
-			fill: false,
-			backgroundColor: 'rgba(75, 192, 192, 0.4)',
-			borderColor: 'rgba(75, 192, 192, 1)',
-	},],};
-	const event = {
-		labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-		datasets: [{
-			label: 'Statistiques',
-			data: [3, 7, 9, 17],
-			fill: false,
-			backgroundColor: 'rgba(75, 192, 192, 0.4)',
-			borderColor: 'rgba(75, 192, 192, 1)',
-	},],};
-	const data = {
-		labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
-		datasets: [{
-			label: 'Statistiques',
-			data: [3, 7, 9, 17],
-			fill: false,
-			backgroundColor: 'rgba(75, 192, 192, 0.4)',
-			borderColor: 'rgba(75, 192, 192, 1)',
-	},],};
-
-	  const options = {
-		responsive: true,
-		maintainAspectRatio: false,
-	  };
     return (
     	<>
-			<Titlebox title=""></Titlebox>
-			<div className="container">
-    			<div className="box">
-    			    <h3 className="title is-4">New customers</h3>
-    			    <div style={{ height: '300px' }}> <Line data={newCustomers} options={options} />
-					</div>
-    			</div>
-    			<div className="box">
-    			    <h3 className="title is-4">Event this month</h3>
-    			    <div style={{ height: '300px' }}> <Line data={event} options={options} />
-					</div>
-    			</div>
-    			<div className="box">
-    			    <h3 className="title is-4">Graphique des Statistiques</h3>
-    			    <div style={{ height: '300px' }}> <Line data={data} options={options} />
-					</div>
-    			</div>
-    		</div>
-			<div className="table-container">
-                <table className="table is-striped is-fullwidth custom-table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Duration</th>
-                            <th>Max Participants</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Event 1</td>
-                            <td>2024-09-05</td>
-                            <td>2 hours</td>
-                            <td>100</td>
-                        </tr>
-                        <tr>
-                            <td>Event 2</td>
-                            <td>2024-09-10</td>
-                            <td>3 hours</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>Event 3</td>
-                            <td>2024-09-15</td>
-                            <td>1.5 hours</td>
-                            <td>200</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+		<Titlebox title=""></Titlebox>
+      	<div className="container">
+        	<div className="box centered-box">
+          		<h3 className="title is-4 dark-text">Number of customers</h3>
+          		<h3 className="subtitle is-2 dark-text">45</h3>
+        	</div>
+        	<div className="box centered-box">
+          		<h3 className="title is-4 dark-text">Number of coaches</h3>
+          		<h3 className="subtitle is-2 dark-text">12</h3>
+        	</div>
+        	<div className="box centered-box">
+          		<h3 className="title is-4 dark-text">Number of events</h3>
+          		<h3 className="subtitle is-2 dark-text">126</h3>
+        	</div>
+      </div>
+
+      <div className="table-container">
+        	<table className="table custom-table-stats">
+          		<thead>
+            		<tr>
+              			<th>Name</th>
+              			<th>Date</th>
+              			<th>Duration</th>
+              			<th>Max Participants</th>
+            		</tr>
+          		</thead>
+          		<tbody>
+            		<tr>
+              			<td>Event 1</td>
+              			<td>2024-09-05</td>
+              			<td>2 hours</td>
+              			<td>100</td>
+            		</tr>
+            		<tr>
+              			<td>Event 2</td>
+              			<td>2024-09-10</td>
+              			<td>3 hours</td>
+              			<td>50</td>
+            		</tr>
+            		<tr>
+              			<td>Event 3</td>
+              			<td>2024-09-15</td>
+              			<td>1.5 hours</td>
+              			<td>200</td>
+            		</tr>
+          		</tbody>
+        	</table>
+      	</div>
       	</>
     )
   };
--- frontend/src/pages/Statistics.tsx
+++ frontend/src/pages/Statistics.tsx
@@ -1,11 +1,124 @@
+import Titlebox from "../components/Titlebox"
+import "./Stats.scss";
+import { Line, Pie } from "react-chartjs-2";
+import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement,
+		Title, Tooltip, Legend,
+		ArcElement, } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, ArcElement,
+	Title, Tooltip, Legend );
 const Statistics = () => {
+	const newCustomers = {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+		datasets: [{
+			label: "Meetings",
+			data: [65, 59, 80, 81, 56, 55],
+			fill: false,
+			borderColor: "rgba(75, 192, 192, 1)",
+	},],};
+	const event = {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+		datasets: [{
+			label: "Event organized",
+			data: [32, 59, 80, 81, 56, 70],
+			fill: false,
+			borderColor: "rgba(75, 192, 192, 1)",
+	},],};
+
+	const astrologicData = {
+		labels: ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Aquarius", "Sagittarius", "Capricorn", "Pisces"],
+		datasets: [
+	  		{
+				data: [12, 19, 3, 5, 2, 3, 8, 10, 4, 12, 20, 9],
+				backgroundColor: [
+		  			"rgba(255, 0, 0, 0.2)",
+		  			"rgba(0, 255, 0, 0.2)",
+		  			"rgba(0, 0, 255, 0.2)",
+		  			"rgba(255, 255, 0, 0.2)",
+		  			"rgba(0, 255, 255, 0.2)",
+		  			"rgba(255, 0, 255, 0.2)",
+					"rgba(255, 165, 0, 0.2)",
+					"rgba(128, 0, 128, 0.2)",
+					"rgba(255, 105, 180, 0.2)",
+					"rgba(165, 42, 42, 0.2)",
+					"rgba(128, 128, 128, 0.2)",
+					"rgba(0, 0, 0, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 0, 0, 0.2)",
+					"rgba(0, 255, 0, 0.2)",
+					"rgba(0, 0, 255, 0.2)",
+					"rgba(255, 255, 0, 0.2)",
+					"rgba(0, 255, 255, 0.2)",
+					"rgba(255, 0, 255, 0.2)",
+				  	"rgba(255, 165, 0, 0.2)",
+				  	"rgba(128, 0, 128, 0.2)",
+				  	"rgba(255, 105, 180, 0.2)",
+				  	"rgba(165, 42, 42, 0.2)",
+				  	"rgba(128, 128, 128, 0.2)",
+				  	"rgba(0, 0, 0, 0.2)",
+				],
+				borderWidth: 1,
+	  		},
+		],
+	};
+
+	const genderData = {
+		labels: ["Women", "Men", "Others"],
+		datasets: [
+	  		{
+				data: [20, 25, 4],
+				backgroundColor: [
+		  			"rgba(255, 105, 180, 0.2)",
+		  			"rgba(0, 255, 255, 0.2)",
+					"rgba(128, 128, 128, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 105, 180, 0.2)",
+					"rgba(0, 255, 255, 0.2)",
+					"rgba(128, 128, 128, 0.2)",
+				],
+				borderWidth: 1,
+	  		},
+		],
+	};
+
+	const options = {
+		responsive: true,
+		maintainAspectRatio: false,
+	};
+
     return (
     	<>
-        	<div className="columns">
-				<div className="column">
-					<h1 className="basic-text-color">Statistics</h1>
-				</div>
+		<Titlebox title="Statistics"></Titlebox>
+      	<div className="container">
+        	<div className="box">
+          		<h3 className="title is-4 dark-text">Event last months</h3>
+          		<div style={{ height: "300px" }}>
+            		<Line data={event} options={options} />
+          		</div>
         	</div>
+			<div className="box">
+				<h3 className="title is-4 dark-text">Astologic signs</h3>
+				<div style={{ height: "300px", width: "300px" }}>
+      				<Pie data={astrologicData} options={options} />
+    			</div>
+			</div>
+			<div className="box">
+				<h3 className="title is-4 dark-text">Genders</h3>
+				<div style={{ height: "300px", width: "300px" }}>
+      				<Pie data={genderData} options={options} />
+    			</div>
+			</div>
+      	</div>
+		<div className="container">
+			<div className="box">
+          		<h3 className="title is-4 dark-text">Encounters last months</h3>
+          		<div style={{ height: "300px" }}>
+            		<Line data={newCustomers} options={options} />
+          		</div>
+        	</div>
+		</div>
       	</>
     )
   };
