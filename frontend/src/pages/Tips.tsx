1df0d51ec355b4d93cb608287446f7c17423f596
Baptiste <baboufe@gmail.com>
2024-09-09 15:37:20 +0200

    feature : started page of tips

--- frontend/src/pages/Tips.tsx
+++ frontend/src/pages/Tips.tsx
@@ -1,20 +1,43 @@
+import "./Tips.scss";
 import Titlebox from "../components/Titlebox";
 
 const Tips = () => {
     return (
     	<>
         	<Titlebox title="Tips"></Titlebox>
-			<div className="container">
+			<div className="box-container">
         		<div className="box">
           			<h3 className="title is-4 dark-text">First Tip</h3>
         		</div>
 				<div className="box">
-					<h3 className="title is-4 dark-text">Seconde Tip</h3>
+					<h3 className="title is-4 dark-text">Second Tip</h3>
 				</div>
 				<div className="box">
 					<h3 className="title is-4 dark-text">Third Tip</h3>
 				</div>
-      		</div>
+			</div>
+			<div className="box-container">
+        		<div className="box">
+          			<h3 className="title is-4 dark-text">First Tip</h3>
+        		</div>
+				<div className="box">
+					<h3 className="title is-4 dark-text">Second Tip</h3>
+				</div>
+				<div className="box">
+					<h3 className="title is-4 dark-text">Third Tip</h3>
+				</div>
+			</div>
+			<div className="box-container">
+        		<div className="box">
+          			<h3 className="title is-4 dark-text">First Tip</h3>
+        		</div>
+				<div className="box">
+					<h3 className="title is-4 dark-text">Second Tip</h3>
+				</div>
+				<div className="box">
+					<h3 className="title is-4 dark-text">Third Tip</h3>
+				</div>
+			</div>
       	</>
     )
   };
