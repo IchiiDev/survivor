b614f449c3bce1f912f141fabeec7c2ba2d7d565
Anthony <anthony.ardhuin@epitech.eu>
2024-09-11 17:07:45 +0200

    feat : plus button for coaches

--- frontend/src/pages/Coaches.tsx
+++ frontend/src/pages/Coaches.tsx
@@ -4,29 +4,52 @@ import { Link } from "react-router-dom";
 
 const Coaches = () => {
 	const [coaches, setCoaches] = useState([]);
-
-	const fetchCoaches = async () => {
-		try {
-		  	const response = await fetch("http://localhost:3001/employees", {
-				method: "GET",
-				headers: {
-				  "Content-Type": "application/json",
-				  "Authorization": `Bearer ${localStorage.getItem("token")}`
-				},
-			});
-		  	if (!response.ok) {
-				throw new Error(`Erreur HTTP: ${response.status}`);
-		  	}
-			const data = await response.json();
-			setCoaches(data);
-		} catch (error) {
-		  console.error("Erreur lors de l'appel API", error);
-		}
-	};
+	const [showPopup, setShowPopup] = useState(false);
 
 	useEffect(() => {
+		const fetchCoaches = async () => {
+			try {
+				  const response = await fetch("http://localhost:3001/employees", {
+					method: "GET",
+					headers: {
+					  "Content-Type": "application/json",
+					  "Authorization": `Bearer ${localStorage.getItem("token")}`
+					},
+				});
+				  if (!response.ok) {
+					throw new Error(`Erreur HTTP: ${response.status}`);
+				  }
+				const data = await response.json();
+				setCoaches(data);
+			} catch (error) {
+			  console.error("Erreur lors de l'appel API", error);
+			}
+		};
 		fetchCoaches();
 	}, []);
+
+	const handleNewCoach = () => {
+		setShowPopup(true);
+  	};
+
+  const handleSubmitPopup = (event: React.MouseEvent<HTMLButtonElement>) => {
+	  	const email = document.getElementById("email-input") as HTMLInputElement;
+	  	const password = document.getElementById("password-input") as HTMLInputElement;
+	  	const name = document.getElementById("name-input") as HTMLInputElement;
+	  	const surname = document.getElementById("surname-input") as HTMLInputElement;
+	  	const gender = document.getElementById("gender-input") as HTMLInputElement;
+	  	const work = document.getElementById("work-input") as HTMLInputElement;
+	  	if (email && password && name && surname && gender && work) {
+		  	console.log("New infos");
+	  	} else {
+			alert("Infos requiered")
+	  	}
+	  	setShowPopup(false);
+  };
+
+  const handleClosePopup = (event: React.MouseEvent<HTMLButtonElement>) => {
+	  setShowPopup(false);
+  }
 
     return (
     	<>
@@ -41,7 +64,7 @@ const Coaches = () => {
 						<span>Export</span>
 					</button>
 					<button className='button is-info'>
-						<img className='icon' src="/assets/icon-plus.svg" alt='add' />
+						<img className='icon' onClick={handleNewCoach} src="/assets/icon-plus.svg" alt='add' />
 					</button>
 				</div>
 			</div>
@@ -112,6 +135,24 @@ const Coaches = () => {
 					</tbody>
 				</table>
 			</div>
+			{showPopup && (
+    		<div className="popup">
+      			<div className="popup-inner">
+        			<h2>New coach:</h2>
+        			<input type="email" id="email-input" placeholder="Email" />
+        			<input type="password" id="password-input" placeholder="Password" />
+        			<input type="name" id="name-input" placeholder="Name" />
+        			<input type="surname" id="surname-input" placeholder="Surname" />
+        			<input type="birthdate" id="birthday-input" placeholder="Birthdate" />
+        			<input type="gender" id="gender-input" placeholder="Male/Female/Other" />
+        			<input type="work" id="work-input" placeholder="Coach/Other" />
+        			<div className="popup-button">
+          				<button onClick={handleClosePopup} style={{color: "red"}}>Cancel</button>
+          				<button onClick={handleSubmitPopup} style={{color: "green"}}>Submit</button>
+        			</div>
+      			</div>
+    		</div>
+  	)}
       	</>
     )
   };
